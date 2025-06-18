183394580eb3a9f72c12ad5505f5bdb15f81310c
Jack Xiao <jackxiao031104@gmail.com>
2025-06-18 01:15:43 +0100

    add kdb

--- kdb.py
+++ kdb.py
@@ -11,7 +11,7 @@ today = np.datetime64(date.today())
 yesterday = today - np.timedelta64(1, 'D')
 event_date_range = [np.datetime64("2024-12-16"), np.datetime64("2025-05-20")]
 
-# q query as string with placeholders
+### Original
 query_to_get_event = """
 ({string connDetails}) (({dtRange};{eventSym})
  select from latestEconomicEvent
@@ -20,11 +20,40 @@ query_to_get_event = """
        eventDate.date within dtRange
 );dtRange;eventSym)
 """
-
-# Run it
 result = to_pd(kx.q(
     query_to_get_event,
     static_data_conn_details,
     event_date_range,
     event
 ))
+
+# new 1
+query_to_get_event = f"""
+{{{static_data_conn_details}}} (({{{event_date_range};`{event}}})
+ select from latestEconomicEvent
+ where sym = `{event},
+       date = .z.D - 1,
+       eventDate.date within {event_date_range}
+);{event_date_range};`{event})
+"""
+result = to_pd(kx.q(query_to_get_event))
+
+
+# new2
+query_to_get_event = """
+({connDetails}) (({dtRange};`$eventSym)
+ select from latestEconomicEvent
+ where sym = `$eventSym,
+       date = .z.D - 1,
+       eventDate.date within dtRange
+);dtRange;`$eventSym)
+"""
+
+result = to_pd(kx.q(
+    query_to_get_event,
+    static_data_conn_details,
+    event_date_range,
+    event
+))
+
+
