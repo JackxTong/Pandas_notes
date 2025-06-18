129a312c79114791232eb8a0cdb4f048fc8c4166
Jack Xiao <jackxiao031104@gmail.com>
2025-06-18 01:10:33 +0100

    add kdb

---
+++ kdb.py
@@ -0,0 +1,30 @@
+import numpy as np
+from datetime import date
+
+# Setup
+database = 's1:29502'  # economic event DB
+static_data_conn_details = f':kdb-rates-gem-{database}:produser:prodpass'
+event = "US Core PCE yy (US)"
+
+# Date filters
+today = np.datetime64(date.today())
+yesterday = today - np.timedelta64(1, 'D')
+event_date_range = [np.datetime64("2024-12-16"), np.datetime64("2025-05-20")]
+
+# q query as string with placeholders
+query_to_get_event = """
+({string connDetails}) (({dtRange};{eventSym})
+ select from latestEconomicEvent
+ where sym = eventSym,
+       date = .z.D - 1,
+       eventDate.date within dtRange
+);dtRange;eventSym)
+"""
+
+# Run it
+result = to_pd(kx.q(
+    query_to_get_event,
+    static_data_conn_details,
+    event_date_range,
+    event
+))
