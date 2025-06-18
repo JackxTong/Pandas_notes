097fc2ca8c06a2ed40b5599aa1ce039ba0f74688
Jack Xiao <jackxiao031104@gmail.com>
2025-06-18 01:21:19 +0100

    add kdb

--- kdb.py
+++ kdb.py
@@ -11,49 +11,25 @@ today = np.datetime64(date.today())
 yesterday = today - np.timedelta64(1, 'D')
 event_date_range = [np.datetime64("2024-12-16"), np.datetime64("2025-05-20")]
 
-### Original
-query_to_get_event = """
-({string connDetails}) (({dtRange};{eventSym})
- select from latestEconomicEvent
- where sym = eventSym,
-       date = .z.D - 1,
-       eventDate.date within dtRange
-);dtRange;eventSym)
-"""
-result = to_pd(kx.q(
-    query_to_get_event,
-    static_data_conn_details,
-    event_date_range,
-    event
-))
 
-# new 1
-query_to_get_event = f"""
-{{{static_data_conn_details}}} (({{{event_date_range};`{event}}})
- select from latestEconomicEvent
- where sym = `{event},
-       date = .z.D - 1,
-       eventDate.date within {event_date_range}
-);{event_date_range};`{event})
-"""
-result = to_pd(kx.q(query_to_get_event))
+from datetime import date
 
+# Values
+event = "US Core PCE yy (US)"
+start_date = "2024.12.16"
+end_date = "2025.05.20"
 
-# new2
-query_to_get_event = """
-({connDetails}) (({dtRange};`$eventSym)
- select from latestEconomicEvent
- where sym = `$eventSym,
-       date = .z.D - 1,
-       eventDate.date within dtRange
-);dtRange;`$eventSym)
-"""
+# Format the q query directly
+query_to_get_event = f'''
+select from latestEconomicEvent 
+where sym = `{"`" + event.replace(" ", "_").replace("(", "").replace(")", "")}`, 
+      date = .z.D - 1, 
+      eventDate.date within ({start_date}; {end_date})
+'''
 
-result = to_pd(kx.q(
-    query_to_get_event,
-    static_data_conn_details,
-    event_date_range,
-    event
-))
-
-
+query_to_get_event = f'''
+select from latestEconomicEvent 
+where sym = $"US Core PCE yy (US)", 
+      date = .z.D - 1, 
+      eventDate.date within ({start_date}; {end_date})
+'''
